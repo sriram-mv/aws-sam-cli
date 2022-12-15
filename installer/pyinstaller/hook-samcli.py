c359bb8145a033ccbe21fba351a3671db0dd591b
Sriram Madapusi Vasudevan <srirammv@amazon.com>
2022-12-14 17:27:24 -0800

    fix: remove `pbr` from hidden imports.

--- installer/pyinstaller/hook-samcli.py
+++ installer/pyinstaller/hook-samcli.py
@@ -10,7 +10,6 @@ datas = (
     + hooks.collect_data_files("samcli")
     + hooks.collect_data_files("samtranslator")
     + hooks.collect_data_files("aws_lambda_builders")
-    + hooks.collect_all("pbr", include_py_files=True)[0]
     + hooks.collect_all("cfnlint", include_py_files=True)[0]
     + hooks.collect_all("jschema_to_python", include_py_files=True)[0]
     + hooks.collect_data_files("text_unidecode")
