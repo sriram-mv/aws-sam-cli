8224bf9e08e5f7b8e74c6957a515d00c17e63b9b
Sriram Madapusi Vasudevan <srirammv@amazon.com>
2022-12-16 10:26:21 -0800

    fix: pyinstaller imports

--- installer/pyinstaller/hook-samcli.py
+++ installer/pyinstaller/hook-samcli.py
@@ -9,11 +9,10 @@ datas = (
         "samcli", include_py_files=True, include_datas=["hook_packages/terraform/copy_terraform_built_artifacts.py"]
     )[0]
     + hooks.collect_all("jschema_to_python", include_py_files=False)[0]
+    + hooks.collect_all("cfnlint", include_py_files=False)[0]
     # Collect ONLY data files.
     + hooks.collect_data_files("samcli")
     + hooks.collect_data_files("samtranslator")
     + hooks.collect_data_files("aws_lambda_builders")
-    + hooks.collect_all("cfnlint", include_py_files=True)[0]
-    + hooks.collect_all("jschema_to_python", include_py_files=True)[0]
     + hooks.collect_data_files("text_unidecode")
 )
